fec562b2f3b906bda8bc1ff758b438b29173c0af
Mirelle Mota <mirelle.motta06@gmail.com>
2023-05-24 22:26:23 -0300

    Java-SpringBoot

---
+++ src/main/java/br/com/erudio/JavaSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootUdemyApplication.class, args);
+	}
+
+}
